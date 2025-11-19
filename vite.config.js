a33dfe814c78150e60e5272bc09362cb0414c8da
wonyoung-R <dylan.yoo.101@gmail.com>
2025-11-19 16:13:40 +0900

    fix: Vite 빌드 설정 수정 - 상대 경로로 변경

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,15 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  base: './', // 상대 경로로 설정
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    rollupOptions: {
+      output: {
+        manualChunks: undefined,
+      },
+    },
+  },
 })
 
